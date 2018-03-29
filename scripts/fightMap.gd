733ae7a805954f3799fe46bcf5da9a1cfb72a95c
chtimy <jeremy.riffet@gmail.com>
2018-03-29 23:37:32 +0200

    on avance dans le framework :D

--- scripts/fightMap.gd
+++ scripts/fightMap.gd
@@ -17,35 +17,36 @@ func init(var map, var tileSize, var overlayTexturePath):
 		for j in range(map.matrix[i].size()):
 			m_matrix[i].append({ value = map.matrix[i][j], tile = Sprite.new(), overlay = Sprite.new() } )
 			var tile = m_matrix[i][j].tile
-			tile.set_texture(load(map.textures[map.matrix[i][j]]))
+			tile.set_texture(load(map.textures[map.matrix[i][j]-1]))
 			tile.set_scale(tileSize/tile.get_texture().get_size())
 			tile.set_position(Vector2(i,j) * tileSize)
 			tile.set_centered(false)
 			add_child(tile)
 			var overlay = m_matrix[i][j].overlay
 			overlay.set_texture(load(overlayTexturePath))
-			overlay.set_scale(tileSize/tile.get_texture().get_size())
+			overlay.set_scale(tileSize/overlay.get_texture().get_size())
 			overlay.set_position(Vector2(i,j) * tileSize)
 			overlay.set_centered(false)
 			overlay.set_visible(false)
 			add_child(overlay)
+	activeCases(Vector2(0,0), STRAIGHT, 2)
 
 func activeCases(var position, var direction, var rangeSize):
 	if direction == STRAIGHT:
 		for i in range(position.x - rangeSize, position.x + rangeSize):
-			enableOverlayCases(i, position.y)
+			enableOverlayCases(Vector2(i, position.y))
 		for j in range(position.y - rangeSize, position.y + rangeSize):
-			enableOverlayCases(position.x, j)
+			enableOverlayCases(Vector2(position.x, j))
 	elif direction == DIAGONAL:
 		for i in range(position.x - rangeSize, position.x + rangeSize):
 			for j in range(position.y - rangeSize, position.y + rangeSize):
-				enableOverlayCases(i, j)
+				enableOverlayCases(Vector2(i, j))
 				i +=1
 	#pas vraiment rond :o
 	elif direction == ROUND:
 		for i in range(position.x - rangeSize, position.x + rangeSize):
 			for j in range(position.y - rangeSize, position.y + rangeSize):
-				enableOverlayCases(i, j)
+				enableOverlayCases(Vector2(i, j))
 
 func enableOverlayCases(var position):
 	if insideMatrix(position):
@@ -65,6 +66,6 @@ func insideMatrix(var position):
 # a completer au fur et a mesure des types de case
 #du moins à modifier et faire en fct des catégories
 func freeCase(var position):
-	if m_matrix[position.x][position.y] == 1:
+	if m_matrix[position.x][position.y].value == 1:
 		return true
 	return false
